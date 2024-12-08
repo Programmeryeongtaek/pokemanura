5eb455effe0b78a8967343c3c37f97bf86eb3d26
YeongTaek <yeongtaek2109@gmail.com>
2024-12-08 21:31:39 +0900

    refactor: 파일 위치 변경

--- app/navigation/MainNavigator.tsx
+++ app/navigation/MainNavigator.tsx
@@ -2,10 +2,10 @@ import { MaterialIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from '../../screens/home/HomeScreen';
-import MetricsScreen from '../../screens/monitoring/MetricsScreen';
-import StatusScreen from '../../screens/status/StatusScreen';
-import ServiceListScreen from '../../screens/management/ServiceListScreen';
+import HomeScreen from '../screens/home/HomeScreen';
+import MetricsScreen from '../screens/monitoring/MetricsScreen';
+import StatusScreen from '../screens/status/StatusScreen';
+import ServiceListScreen from '../screens/management/ServiceListScreen';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
