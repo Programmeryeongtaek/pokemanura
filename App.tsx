de5a650f71021cf4612d7091d14ae7f97d267c3a
YeongTaek <yeongtaek2109@gmail.com>
2024-12-09 18:55:41 +0900

    feat: 네비게이션 Stack 설정

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainNavigator from './app/navigation/MainNavigator';
 import { AuthProvider } from './contexts/AuthContext';
+import { RootStackParamList } from './types/navigation';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './app/screens/home/HomeScreen';
+import LoginScreen from './app/screens/home/LoginScreen';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <AuthProvider>
-      <MainNavigator />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: '홈' }}
+          />
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ title: '로그인' }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </AuthProvider>
   );
 }
