f1fa4709581b2ec1a338226ca0e796b884026863
YeongTaek <yeongtaek2109@gmail.com>
2024-12-09 14:30:19 +0900

    feat: 토큰 저장 유틸리티

---
+++ utils/storage.ts
@@ -0,0 +1,15 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AuthTokens } from '../types/auth';
+
+export const storeTokens = async (tokens: AuthTokens) => {
+  try {
+    await AsyncStorage.multiSet([
+      ['access_token', tokens.access_token],
+      ['refresh_token', tokens.refresh_token],
+      ['token_type', tokens.token_type],
+    ])
+  } catch (error)  {
+    console.error('Error storing tokens:', error);
+    throw error;
+  }
+}
